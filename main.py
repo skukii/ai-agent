f058b15861dc76049be6ddaec555e92ad90caf91
skukii <ana.skuks@gmail.com>
2025-09-17 21:22:11 +0200

    added api call

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    )
+
+
 
 
 if __name__ == "__main__":
