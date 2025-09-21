5f862fee1d7fe8dafdded16e07a2e22833b8cc73
skukii <ana.skuks@gmail.com>
2025-09-21 10:41:49 +0200

    added a calculator

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
+
 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
+    arguments = sys.argv
+    
+    user_prompt = arguments[1]
+
+    if len(arguments)>2:
+        if str(arguments[2]) == "--verbose":
+            ver = True
+    else:
+        ver = False
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]  
+
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
+    model='gemini-2.0-flash-001', contents=messages
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if ver:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 
